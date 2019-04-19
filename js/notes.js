f079109edd24e47e4f7b47a86ccb0c1f600b2ff2
qiulang@macbook3 <qiulang2000@hotmail.com>
2019-04-19 18:48:25 +0800

    v-for

--- js/notes.js
+++ js/notes.js
@@ -1,3 +1,10 @@
 var app = new Vue({
-  el: "#app"
+  el: "#app",
+  data: {
+    notes: [
+      { id: 1, body: "This is a first test", timestamp: Date.now() },
+      { id: 2, body: "This is a second test", timestamp: Date.now() },
+      { id: 3, body: "This is a third test", timestamp: Date.now() }
+    ]
+  }
 });
